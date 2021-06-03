19d13345e00428b8ebf43546c619bfa74bf0a668
Vignesh C <poppyseed6464@outlook.com>
2021-06-03 14:43:36 +0530

    now adding trial controller

---
+++ WebApiSchool/Controllers/trailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiSchool.Controllers
+{
+    public class trailController : Controller
+    {
+        // GET: trail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
