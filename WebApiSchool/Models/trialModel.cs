001976564243da26a66700d54cba24b9cd552e76
Vignesh C <poppyseed6464@outlook.com>
2021-06-03 14:58:27 +0530

    add classs

---
+++ WebApiSchool/Models/trialModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiSchool.Models
+{
+    public class trialModel
+    {
+        public void Get()
+        {
+
+        }
+    }
+}
